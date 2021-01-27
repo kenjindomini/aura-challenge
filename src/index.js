eedc5ba525ef82230bbd109353db4b5ff3c4ed7d
Keith Olenchak <kenjin.domini@gmail.com>
2021-01-26 20:45:19 -0500

    start blocking out basic structure of api handler

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
 // lambda-like handler function
 module.exports.handler = async event => {
-  // do stuff...
+  switch (event.path) {
+    case "/zipcodes": {
+      return await zipcodesHandler(
+        event.httpMethod,
+        event.headers,
+        event.queryStringParameters,
+        event.body
+      );
+    }
+    case "/zipcode": {
+      return await zipcodeHandler(
+        event.httpMethod,
+        event.headers,
+        event.queryStringParameters,
+        event.body
+      );
+    }
+    default: {
+      throw new Error(`Unknown path: ${event.path}`);
+    }
+  }
 };
+
+async function zipcodesHandler(method, headers, queryString, body) {
+  switch (method) {
+    case "GET": {
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported HTTP Method: ${method}`);
+    }
+  }
+}
+
+async function zipcodeHandler(method, headers, queryString, body) {
+  switch (method) {
+    case "POST": {
+      break;
+    }
+    default: {
+      throw new Error(`Unsupported HTTP Method: ${method}`);
+    }
+  }
+}
--- src/test.js
+++ src/test.js
@@ -4,4 +4,52 @@ describe("basic tests", () => {
   test("handler function exists", () => {
     expect(typeof handler).toBe("function");
   });
+  test("Unknown path throws an error", async () => {
+    var event = {
+      httpMethod: "GET",
+      path: "/resource",
+      headers: {},
+      queryStringParameters: {
+        date: "2020-11-13"
+      }
+    };
+    expect.assertions(1);
+    await expect(handler(event)).rejects.toEqual(
+      Error("Unknown path: /resource")
+    );
+  });
+});
+
+describe("/zipcodes tests", () => {
+  test("Unsupported method throws an error", async () => {
+    var event = {
+      httpMethod: "POST",
+      path: "/zipcodes",
+      headers: {},
+      queryStringParameters: {
+        date: "2020-11-13"
+      }
+    };
+    expect.assertions(1);
+    await expect(handler(event)).rejects.toEqual(
+      Error("Unsupported HTTP Method: POST")
+    );
+  });
+});
+
+describe("/zipcode tests", () => {
+  test("Unsupported method throws an error", async () => {
+    var event = {
+      httpMethod: "PATCH",
+      path: "/zipcode",
+      headers: {},
+      queryStringParameters: {
+        date: "2020-11-13"
+      }
+    };
+    expect.assertions(1);
+    await expect(handler(event)).rejects.toEqual(
+      Error("Unsupported HTTP Method: PATCH")
+    );
+  });
 });
